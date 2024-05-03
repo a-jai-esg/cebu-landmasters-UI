ac19b4c9e1e62d88e09fb9da409cbfbcac67aaff
Elijah Nicholas Esguerra <ashesg1117@gmail.com>
2024-05-04 00:00:29 +0800

    started entity filters

--- src/components/global/Sidebar/FilterComponent/index.tsx
+++ src/components/global/Sidebar/FilterComponent/index.tsx
@@ -7,13 +7,17 @@ import { Box } from "@mui/material";
 const FilterComponent = () => {
     return (
         <>
+        {/* Filters Subheader */}
             <div style={{ display: "flex", alignItems: "center", margin: "15px 0 5px 20px" }}>
                 <FilterAltOutlined sx={{ paddingRight: "15px", color: "#c1c5de", fontWeight: 600 }} />
                 <Typography variant="h6" color="#c1c5de" fontWeight="600" fontSize={14}>
                     Filters
                 </Typography>
             </div>
-            <div style={{ marginTop: "10px", marginLeft: "25px", marginRight: "25px" }}>
+
+        {/* Date Filters */}
+            <div style={{ marginTop: "20px", marginLeft: "25px", marginRight: "25px" }}>
+                {/* Start Date */}
                 <Box sx={{ marginBottom: "10px" }}>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DatePicker
@@ -29,7 +33,8 @@ const FilterComponent = () => {
                         />
                     </LocalizationProvider>
                 </Box>
-                <Box sx={{ marginBottom: "10px" }}>
+                {/* End Date */}
+                <Box sx={{marginTop: "20px", marginBottom: "20px" }}>
                     <LocalizationProvider dateAdapter={AdapterDayjs}>
                         <DatePicker
                             label="End Date"
@@ -44,6 +49,30 @@ const FilterComponent = () => {
                         />
                     </LocalizationProvider>
                 </Box>  
+            </div>
+
+        {/* Entity Filters */}
+            <div style={{ display: "flex", alignItems: "center", margin: "35px 0 5px 20px" }}>
+                <Typography variant="h6" color="#c1c5de" fontWeight="600" fontSize={14}>
+                ENTITY
+                </Typography>
+            </div>
+            <div style={{ marginTop: "20px", marginLeft: "25px", marginRight: "25px" }}>
+                <Box sx={{ marginBottom: "10px" }}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DatePicker
+                            label="Start Date"
+                            sx={{
+                                fontWeight: 400,
+                                fontSize: "inherit",
+                                color: "#c1c5de",
+                                '& input': {
+                                    fontSize: "8px",
+                                }
+                            }}
+                        />
+                    </LocalizationProvider>
+                </Box>
             </div>
         </>
     );
