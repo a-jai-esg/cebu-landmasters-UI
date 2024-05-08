c99d791ea0b1138ce44ddc6651ec8ae9494d3d82
Elijah Nicholas Esguerra <ashesg1117@gmail.com>
2024-05-09 00:27:02 +0800

    added lodash and initial data calculation classes.

---
+++ src/data/data-calculation/opex/index.ts
@@ -0,0 +1,10 @@
+import _ from "lodash";
+// import OperatingExpenses from "../../data-interfaces/OperatingExpenses/operatingExpenses";
+
+export default class opexCalculations {
+  constructor() {}
+
+  // calculateTotalOpex = (data[] : any): number => {
+
+  // }
+}
