4e05c64af849ccca8c04184ed333525985a3ccc0
janinevallente <janinexvallente@gmail.com>
2024-06-04 19:42:35 +0800

    enh: changed expand chart into expand table

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -298,7 +298,7 @@ const Dashboard: React.FC<DashboardProps> = ({
                       fontSize: "0.875rem",
                     }}
                   >
-                    Expand Chart
+                    Expand Table
                   </Typography>
                 </CardContent>
               </Card>
