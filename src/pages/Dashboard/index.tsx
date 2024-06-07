e94b575e40d1da0812a1f25ebfae6dee3df744a4
janinevallente <janinexvallente@gmail.com>
2024-06-07 19:53:30 +0800

    enh: adjusted heights

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -238,7 +238,7 @@ const Dashboard: React.FC<DashboardProps> = ({
           {/* Pie Chart*/}
           {cardTitles.length > 2 && (
             <Grid item xs={12} sm={6} md={4}>
-              <Card sx={{ borderRadius: 3, boxShadow: 6, height: "fit" }}>
+              <Card sx={{ borderRadius: 3, boxShadow: 6, height: 422 }}>
                 <CardContent>
                   <PrimaryPieChartComponent
                     pieData={pieDataFiltered}
