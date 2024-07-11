59c56a894b4a4bcdf1cfdba63620cdbd36418af6
janine-myt <janine@mytdevcorp.com>
2024-07-11 22:15:29 +0800

    enha: enhanced responsiveness

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -191,7 +191,7 @@ const Dashboard: React.FC<DashboardProps> = ({
           )}
           {/* Bar chart */}
           {cardTitles.length > 1 && (
-              <Grid item sm={12} md={4}>
+              <Grid item xs={12} md={6} lg={4}>
               <Card sx={{ borderRadius: 3, boxShadow: 6, width:"100%" }}>
                 <CardContent>
                   <BarchartComponent
@@ -216,7 +216,7 @@ const Dashboard: React.FC<DashboardProps> = ({
           )}
           {/* Pie Chart*/}
           {cardTitles.length > 2 && (
-            <Grid item sm={12} md={4}>
+            <Grid item xs={12} md={6} lg={4}>
               <Card sx={{ borderRadius: 3, boxShadow: 6, height:"100%" }}>
                 <CardContent>
                   <PrimaryPieChartComponent
@@ -243,7 +243,7 @@ const Dashboard: React.FC<DashboardProps> = ({
           {/* Income Statement Table */}
           {/* Table*/}
           {cardTitles.length > 3 && (
-            <Grid item sm={12} md={4}>
+            <Grid item xs={12} md={12} lg={4}>
               <Card sx={{ borderRadius: 3, boxShadow: 6 }}>
                 <CardContent>
                   <Typography fontSize={20} color="#333" fontWeight="bold">
